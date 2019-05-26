ff0a013b75c774eb0adc446a374068ddcb12f69a
Federico Lochbaum <fede@SCVsofts-MacBook-Pro-2.local>
2019-05-26 15:21:53 -0300

    config basic mongoose and very simple user schema

--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
 import express from 'express'
 import api from './routes/api'
+import mongoose from 'mongoose'
+import User from './schema/userSchema'
+
+await mongoose.connect(
+  // 'mongodb://localhost/myappdatabase'
+)
 
 const app = express()
 
-app.use('/api', api)
+const userExample = new User({
+  name: 'user',
+  username: 'juanito',
+  password: 'password'
+})
 
+app.use('/api', api)
 app.get('/', (req, res) => {
   res.status(200).send('hello')
 })
