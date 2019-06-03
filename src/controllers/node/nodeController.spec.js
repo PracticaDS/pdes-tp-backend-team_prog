ae720123cbb63a6557446698915a5432279af747
LSExiLio <sandoval.lucasj@gmail.com>
2019-06-02 23:38:02 -0300

    Add - node controller

---
+++ src/controllers/node/nodeController.spec.js
@@ -0,0 +1,12 @@
+import request from 'supertest'
+import app from '../server'
+
+import NodeCtrl from './nodeController'
+
+describe('CREATE node', () => {
+
+  it('requesting /api gives a status ok', () => {
+    NodeCtrl.
+  })
+
+})
--- src/schema/models.js
+++ src/schema/models.js
@@ -2,7 +2,12 @@ import mongoose from 'mongoose'
 import User from './userSchema'
 import NodeBlock  from './nodeSchema'
 
-export const connectDb = () => mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/game')
+const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/game'
+
+export const connectDb = () => 
+  mongoose.connect(DATABASE_URL, (err) => {
+    console.log('An error has ocurred trying to connect to: ' + DATABASE_URL)
+  })
 
 const models = { User, NodeBlock }
 
