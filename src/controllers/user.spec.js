5c6985967ae005a0fa7dc3348d49ea1ed0cb05d5
Federico Lochbaum <fede@SCVsofts-MacBook-Pro-2.local>
2019-06-06 09:17:03 -0300

    adding a test case

--- src/controllers/user.spec.js
+++ src/controllers/user.spec.js
@@ -8,7 +8,6 @@ import User from '../schema/user/user'
 import userRoutes from '../routes/user'
 
 describe('Routes for User', () => {
-
   const app = express()
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
@@ -36,6 +35,29 @@ describe('Routes for User', () => {
         //Here only expect that `_id` field exists, since mockingoose sets a value and i cant find a way to control it
         expect(response.body.res._id)
         expect(response.body.res.username).toEqual(randomUsername)
+        expect(response.body.res.factories).toEqual([])
+      })
+  })
+
+  it('GET /user/:userId', async () => {
+    const id = 1
+    const username = 'bla'
+    const user = {
+      id,
+      username,
+      factories: []
+    }
+
+    mockingoose(User).toReturn(user, 'findById')
+
+    return request(app)
+      .get(`/user/${id}`)
+      .expect(200)
+      .then(response => {
+        expect(response)
+        expect(response.body)
+        expect(response.body.res._id).toEqual(id)
+        expect(response.body.res.username).toEqual(username)
         expect(response.body.res.factories).toEqual([])
       })
   })
