7584b08db89a0b2ec3ad12e29cb7e1575adf3ce9
LSExiLio <sandoval.lucasj@gmail.com>
2019-06-06 02:05:43 -0300

    Add - api test, Fix - how to use a request controller

---
+++ src/controllers/user.spec.js
@@ -0,0 +1,42 @@
+
+import mockingoose from 'mockingoose'
+import express from 'express'
+import bodyParser from 'body-parser'
+import request from 'supertest'
+
+import User from '../schema/user/user'
+import userRoutes from '../routes/user'
+
+describe('Routes for User', () => {
+
+  const app = express()
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use('/user', userRoutes)
+
+  it('POST /user/', async () => {
+    const _user = (someUsername) => {
+      return {
+        username: someUsername,
+        factories: []
+    }}
+
+    // We mock mongo here
+    mockingoose(User).toReturn(_user, 'create')
+
+    const randomUsername = (Math.random() * 10000).toString()
+    return request(app)
+      .post('/user')
+      .send({username: randomUsername})
+      .expect(200)
+      .then(response => {
+        expect(response)
+        expect(response.body)
+
+        //Here only expect that `_id` field exists, since mockingoose sets a value and i cant find a way to control it
+        expect(response.body.res._id)
+        expect(response.body.res.username).toEqual(randomUsername)
+        expect(response.body.res.factories).toEqual([])
+      })
+  })
+})
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,17 +1,53 @@
 import User from '../schema/user/user'
-import connection from '../schema/models'
 
- export const createUser = (req, res) => connection.then(() => {
-  const { username } = req.params
-  new User({  username,  factories: []}).save().then(user => { res.send(user) })
-}).catch(() => {})
+export const createUser = (req, res) => {
+  // Parse the request... here we need the body from the request
+  const { body } = req
 
- export const getUser = (req, res) => connection.then(() => {
-  const { id } = req.params
-  User.findById(id).then(user => {
-    res.send(user)
-  })
-}).catch(() => {})
+  // We decide what interaction with the database we want to do
+  User.create(
+    
+    // Create the body for the operation... here is a create so, construct the User, this should be
+    // in a differente type of file because it has nothing to do with how to parse the request
+    {username: body.username,
+    factories: []}
+  
+  )
+  // When everything is OK, we send a successful response , with body, res: user_created
+  .then(user_created => res.status(200).send({
+    type: 'Success',
+    res: user_created
+  }))
+  // When there is a problem, we send a Bad Request (just as an example) with body {type: error, body: err}
+  .catch(err => res.status(400).send({
+    type: 'Error',
+    body: err
+  }))
+}
+
+export const getUser = (req, res) => {
+  // Parse the request... here we need the params... remember this responds to a GET /user/:user_id
+  const { params } = req
+
+  // We decide what interaction with the database we want to do
+  User.findById(
+    
+    // Create the body for the operation... so the id is enough (We need to remember, the route definition)
+    // In this case GET /user/:userId so, we search for userId
+    params.userId
+    
+  )
+  // When everything is OK, we send a successful response, with body, res: user
+  .then(user => res.status(200).send({
+    type: 'Success',
+    res: user
+  }))
+  // When there is a problem, we send a Bad Request (just as an example) with body {type: error, body: err}
+  .catch(err => res.status(400).send({
+    type: 'Error',
+    body: err
+  }))
+}
 
  export const updateUser = (req, res) => connection.then(() => {
   const { id, username, factories } = req.params
